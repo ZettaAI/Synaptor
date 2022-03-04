46b5258b317c16404219afad1fc75fecf2586e14
NT <nturner@zetta.ai>
2022-03-04 13:13:10 -0500

    feat: plugging entrypoint.sh into scripts

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -12,48 +12,48 @@
 
 case $1 in
     # base tasks
-    chunk_ccs)        echo chunk_ccs.py ${@:2} ;;
-    merge_ccs)        echo merge_ccs.py ${@:2} ;;
-    match_contins)    echo match_contins.py ${@:2} ;;
-    seg_graph_ccs)    echo seg_graph_ccs.py ${@:2} ;;
-    chunk_seg_map)    echo chunk_seg_map.py ${@:2} ;;
-    merge_seginfo)    echo merge_seginfo.py ${@:2} ;;
-    chunk_edges)      echo chunk_edges.py ${@:2} ;;
-    pick_edge)        echo pick_edge.py ${@:2} ;;
-    merge_dups)       echo merge_dups.py ${@:2} ;;
-    remap_ids)        echo remap_ids.py ${@:2} ;;
-    chunk_overlaps)   echo chunk_overlaps.py ${@:2} ;;
-    merge_overlaps)   echo merge_overlaps.py ${@:2} ;;
-    chunk_anchors)    echo chunk_anchors.py ${@:2} ;;
-    create_index)     echo create_index.py ${@:2} ;;
-    dedup_chunk_segs) echo dedup_chunk_segs.py ${@:2} ;;
-    init_db)          echo init_db.py ${@:2} ;;
+    chunk_ccs)        python3 -u chunk_ccs.py ${@:2} ;;
+    merge_ccs)        python3 -u merge_ccs.py ${@:2} ;;
+    match_contins)    python3 -u match_contins.py ${@:2} ;;
+    seg_graph_ccs)    python3 -u seg_graph_ccs.py ${@:2} ;;
+    chunk_seg_map)    python3 -u chunk_seg_map.py ${@:2} ;;
+    merge_seginfo)    python3 -u merge_seginfo.py ${@:2} ;;
+    chunk_edges)      python3 -u chunk_edges.py ${@:2} ;;
+    pick_edge)        python3 -u pick_edge.py ${@:2} ;;
+    merge_dups)       python3 -u merge_dups.py ${@:2} ;;
+    remap_ids)        python3 -u remap_ids.py ${@:2} ;;
+    chunk_overlaps)   python3 -u chunk_overlaps.py ${@:2} ;;
+    merge_overlaps)   python3 -u merge_overlaps.py ${@:2} ;;
+    chunk_anchors)    python3 -u chunk_anchors.py ${@:2} ;;
+    create_index)     python3 -u create_index.py ${@:2} ;;
+    dedup_chunk_segs) python3 -u dedup_chunk_segs.py ${@:2} ;;
+    init_db)          python3 -u init_db.py ${@:2} ;;
 
     # worker
-    worker)           echo worker.py ${@:2} ;;
+    worker)           python3 -u worker.py ${@:2} ;;
 
     # sanity_check
-    sanity_check)     echo sanity_check.py ${@:2} ;;
+    sanity_check)     python3 -u sanity_check.py ${@:2} ;;
 
     # generate - branches again
     generate)
         case $2 in
-            chunk_ccs)        echo generation/chunk_ccs.py ${@:2} ;;
-            merge_ccs)        echo generation/merge_ccs.py ${@:2} ;;
-            match_contins)    echo generation/match_contins.py ${@:2} ;;
-            seg_graph_ccs)    echo generation/seg_graph_ccs.py ${@:2} ;;
-            chunk_seg_map)    echo generation/chunk_seg_map.py ${@:2} ;;
-            merge_seginfo)    echo generation/merge_seginfo.py ${@:2} ;;
-            chunk_edges)      echo generation/chunk_edges.py ${@:2} ;;
-            pick_edge)        echo generation/pick_edge.py ${@:2} ;;
-            merge_dups)       echo generation/merge_dups.py ${@:2} ;;
-            remap_ids)        echo generation/remap_ids.py ${@:2} ;;
-            chunk_overlaps)   echo generation/chunk_overlaps.py ${@:2} ;;
-            merge_overlaps)   echo generation/merge_overlaps.py ${@:2} ;;
-            chunk_anchors)    echo generation/chunk_anchors.py ${@:2} ;;
-            create_index)     echo generation/create_index.py ${@:2} ;;
-            dedup_chunk_segs) echo generation/dedup_chunk_segs.py ${@:2} ;;
-            init_db)          echo generation/init_db.py ${@:2} ;;
+            chunk_ccs)        python3 -u generation/chunk_ccs.py ${@:2} ;;
+            merge_ccs)        python3 -u generation/merge_ccs.py ${@:2} ;;
+            match_contins)    python3 -u generation/match_contins.py ${@:2} ;;
+            seg_graph_ccs)    python3 -u generation/seg_graph_ccs.py ${@:2} ;;
+            chunk_seg_map)    python3 -u generation/chunk_seg_map.py ${@:2} ;;
+            merge_seginfo)    python3 -u generation/merge_seginfo.py ${@:2} ;;
+            chunk_edges)      python3 -u generation/chunk_edges.py ${@:2} ;;
+            pick_edge)        python3 -u generation/pick_edge.py ${@:2} ;;
+            merge_dups)       python3 -u generation/merge_dups.py ${@:2} ;;
+            remap_ids)        python3 -u generation/remap_ids.py ${@:2} ;;
+            chunk_overlaps)   python3 -u generation/chunk_overlaps.py ${@:2} ;;
+            merge_overlaps)   python3 -u generation/merge_overlaps.py ${@:2} ;;
+            chunk_anchors)    python3 -u generation/chunk_anchors.py ${@:2} ;;
+            create_index)     python3 -u generation/create_index.py ${@:2} ;;
+            dedup_chunk_segs) python3 -u generation/dedup_chunk_segs.py ${@:2} ;;
+            init_db)          python3 -u generation/init_db.py ${@:2} ;;
             *)  echo "invalid task name: $2"; false
 	esac
     ;;
