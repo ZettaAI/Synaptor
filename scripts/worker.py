90e95173d09f5d263e0685f93be5c32c4b94e663
NT <nturner@zetta.ai>
2022-03-11 11:29:53 -0500

    feat: worker script also takes config as arg

--- scripts/worker.py
+++ scripts/worker.py
@@ -6,13 +6,29 @@ import argparse
 from taskqueue import TaskQueue
 
 import synaptor.proc.io.tasks_w_io  # "Registers" tasks for taskqueue
+from synaptor.cloud import parser
 
 
-parser = argparse.ArgumentParser()
+def main(queueurl, config, lease_seconds):
 
-parser.add_argument("qurl", type=str)
-parser.add_argument("lease_seconds", type=int, default=300)
+    if config is not None:
+        queueurl = parser.parse(configfilename)["queueurl"]
+    elif queueurl is not None:
+        pass
+    else:
+        raise Exception("Need to define queueurl or configfilename")
 
-args = parser.parse_args()
-with TaskQueue(qurl=args.qurl, n_threads=0) as tq:
-    tq.poll(lease_seconds=args.lease_seconds)
+    with TaskQueue(qurl=queueurl, n_threads=0) as tq:
+        tq.poll(lease_seconds=lease_seconds)
+
+
+if __name__ == '__main__':
+    ap = argparse.ArgumentParser()
+
+    ap.add_argument("--queueurl", type=str, default=None, help="queue URL")
+    ap.add_argument("--config", type=str, default=None, help="configuration file")
+    ap.add_argument("--lease_seconds", type=int, default=300)
+
+    args = ap.parse_args()
+
+    main(**vars(args))
