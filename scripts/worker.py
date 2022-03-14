bf7e6582236354f7fbb8194f25cb07f875e6ac25
NT <nturner@zetta.ai>
2022-03-14 10:39:30 -0400

    fix: linting worker.py

--- scripts/worker.py
+++ scripts/worker.py
@@ -9,9 +9,9 @@ import synaptor.proc.tasks_w_io  # "Registers" tasks for taskqueue
 from synaptor.cloud import parser
 
 
-def main(queueurl, config, lease_seconds):
+def main(queueurl, configfilename, lease_seconds):
 
-    if config is not None:
+    if configfilename is not None:
         queueurl = parser.parse(configfilename)["queueurl"]
     elif queueurl is not None:
         pass
@@ -22,11 +22,13 @@ def main(queueurl, config, lease_seconds):
         tq.poll(lease_seconds=lease_seconds)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     ap = argparse.ArgumentParser()
 
     ap.add_argument("--queueurl", type=str, default=None, help="queue URL")
-    ap.add_argument("--config", type=str, default=None, help="configuration file")
+    ap.add_argument(
+        "--configfilename", type=str, default=None, help="configuration file"
+    )
     ap.add_argument("--lease_seconds", type=int, default=300)
 
     args = ap.parse_args()
