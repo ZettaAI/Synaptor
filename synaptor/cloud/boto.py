92911fa936d4752f041cd4def374001f3b373570
NT <nturner@zetta.ai>
2022-03-14 14:14:46 -0400

    feat: boto file writer hack

---
+++ synaptor/cloud/boto.py
@@ -0,0 +1,78 @@
+"""A small hack to write boto files with required information for gsutil.
+
+I should probably just work without gsutil soon...
+"""
+from __future__ import annotations
+
+import os
+import json
+from textwrap import dedent
+from collections import namedtuple
+
+
+AWSKeys = namedtuple("AWSKeys", ["access_key_id", "secret_access_key"])
+HOME = os.path.expanduser("~")
+AWSPATH = os.path.join(HOME, ".cloudvolume/secrets", "aws-secret.json")
+GCPPATH = os.path.join(HOME, ".cloudvolume/secrets", "google-secret.json")
+BOTOPATH = os.path.join(HOME, ".boto")
+
+
+def writeboto() -> None:
+    """Writes a boto file for gsutil using the CloudVolume secret files."""
+    awskeys = read_aws_keys() if os.path.isfile(AWSPATH) else None
+    projectid = read_gcp_project() if os.path.isfile(GCPPATH) else None
+
+    if not os.path.isfile(BOTOPATH):
+        _writeboto(BOTOPATH, awskeys, projectid)
+
+
+def read_aws_keys(path: str = AWSPATH):
+    """Reads the AWS credential keys from the secret json file."""
+    with open(path) as f:
+        content = json.load(f)
+
+    return AWSKeys(content["AWS_ACCESS_KEY_ID"], content["AWS_SECRET_ACCESS_KEY"])
+
+
+def read_gcp_project(path: str = GCPPATH):
+    """Reads the google cloud project name from the secret json file."""
+    with open(path) as f:
+        content = json.load(f)
+
+    return content["project_id"]
+
+
+def _writeboto(path: str, awskeys: AWSKeys, projectid: str) -> None:
+    with open(path, "w+") as f:
+        f.write(dedent("[Credentials]\n\n"))
+
+        if awskeys is not None:
+            f.write(f"aws_access_key_id = {awskeys.access_key_id}\n\n")
+            f.write(f"aws_secret_acces_key = {awskeys.secret_access_key}\n\n")
+
+        f.write(f"gs_service_key_file = {GCPPATH}\n\n")
+
+        f.write(
+            dedent(
+                """
+                [Boto]
+
+                https_validate_certificates = True
+
+
+                [GoogleCompute]
+
+                [GSUtil]
+
+                content_language = en
+
+                default_api_version = 2
+
+                """
+            )
+        )
+
+        if projectid is not None:
+            f.write(f"default_project_id = {projectid}\n\n")
+
+        f.write("[OAuth2]\n")
