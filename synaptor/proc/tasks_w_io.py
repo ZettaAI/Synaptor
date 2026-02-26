4d7ca7b71ae111710a63abb37dbaadbe10cecb7a
Alex Bae <shighnealex@gmail.com>
2026-02-26 16:31:59 +0900

    Debugging root segmentation

--- synaptor/proc/tasks_w_io.py
+++ synaptor/proc/tasks_w_io.py
@@ -562,19 +562,8 @@ def edge_task(
             maxmip=aggmaxmip,
         )
 
-    # if root_seg_cvname is not None:
-    #     roots = timed(
-    #         f"Reading root segmentation chunk at {resolution}",
-    #         io.read_cloud_volume_chunk,
-    #         root_seg_cvname,
-    #         chunk_bounds,
-    #         resolution=resolution,
-    #         parallel=parallel,
-    #     )
-    #     assert roots.shape == seg.shape, "mismatched root segmentation"
-    # else:
-    #     roots = None
-    roots = timed(
+    if root_seg_cvname is not None:
+        roots = timed(
             f"Reading root segmentation chunk at {resolution}",
             io.read_cloud_volume_chunk,
             root_seg_cvname,
@@ -582,6 +571,17 @@ def edge_task(
             resolution=resolution,
             parallel=parallel,
         )
+        # assert roots.shape == seg.shape, "mismatched root segmentation"
+    else:
+        roots = None
+    # roots = timed(
+    #         f"Reading root segmentation chunk at {resolution}",
+    #         io.read_cloud_volume_chunk,
+    #         root_seg_cvname,
+    #         chunk_bounds,
+    #         resolution=resolution,
+    #         parallel=parallel,
+    #     )
 
     assoc_net = timed(
         "Reading association network",
@@ -1046,19 +1046,8 @@ def anchor_task(
         parallel=parallel,
     )
 
-    # if root_seg_cvname is not None:
-    #     roots = timed(
-    #         f"Reading root segmentation chunk at mip {seg_mip}",
-    #         io.read_cloud_volume_chunk,
-    #         root_seg_cvname,
-    #         chunk_bounds,
-    #         resolution=seg_mip,
-    #         parallel=parallel,
-    #     )
-    #     assert roots.shape == seg.shape, "mismatched root segmentation"
-    # else:
-    #     roots = None
-    roots = timed(
+    if root_seg_cvname is not None:
+        roots = timed(
             f"Reading root segmentation chunk at mip {seg_mip}",
             io.read_cloud_volume_chunk,
             root_seg_cvname,
@@ -1066,6 +1055,17 @@ def anchor_task(
             resolution=seg_mip,
             parallel=parallel,
         )
+        # assert roots.shape == seg.shape, "mismatched root segmentation"
+    else:
+        roots = None
+    # roots = timed(
+    #         f"Reading root segmentation chunk at mip {seg_mip}",
+    #         io.read_cloud_volume_chunk,
+    #         root_seg_cvname,
+    #         chunk_bounds,
+    #         resolution=seg_mip,
+    #         parallel=parallel,
+    #     )
 
     edge_df = timed("Reading full edge info", taskio.read_full_info, storagestr)
 
